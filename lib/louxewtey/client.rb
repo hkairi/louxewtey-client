7f66d9868b26f6dd489973ab973e500fd84ce77b
Hassane Moustapha <hassanemoustapha@gmail.com>
2011-01-05 11:05:51 +0000

    mise à jour

--- lib/louxewtey/client.rb
+++ lib/louxewtey/client.rb
@@ -10,9 +10,14 @@ module LouXewTey
       return "c'est fait"
     end
     ####################################################################    
-    def get_list_of( resource="", format="xml" )
+    def get_list_of( args = {} )      
+      return nil unless args[:resource]      
+      resource = args[:resource].downcase
+      format   = args[:format].nil? ? "xml" : args[:format].to_s
+      type     = args[:type].nil? ? "" : "?type=#{args[:type].to_s}"
+      type = "?genre=#{args[:resource]}" if args[:resource].downcase == "hotels"
       if %w(evenements hotels).include?( resource )
-        RestClient.get("#{SERVEUR}/#{resource}.#{format}")do |response,request|
+        RestClient.get("#{SERVEUR}/#{resource}.#{format}#{type}")do |response,request|
           return response.body if response.code.to_i == 200
         end
       end
